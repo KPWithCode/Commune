6b8c15ac3e742bc2d87bfdc0e31519f7dab2c464
Kyle Phillips <kphillips375@yahoo.com>
2020-11-12 20:58:57 -0500

    Fiberv2 server

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+
+
+func main() {
+
+	app := fiber.New()
+	// app.Use(logger.New())
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+    })
+
+    app.Listen(":5000")
+}
